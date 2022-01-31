6bb82ea49c17bfa7db069e49ad3c14d48c251886
Jeongs <kimm.jeongs@gmail.com>
2022-01-31 16:30:50 +0900

    :heavy_plus_sign:  Dep: 스토리북 설치 및 절대경로 설정

---
+++ .storybook/main.js
@@ -0,0 +1,19 @@
+const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
+
+module.exports = {
+  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/preset-create-react-app',
+  ],
+  framework: '@storybook/react',
+  core: {
+    builder: 'webpack5',
+  },
+  webpackFinal: async (config) => {
+    // @NOTE: 스토리북 절대경로 설정
+    config.resolve.plugins.push(new TsconfigPathsPlugin({}));
+    return config;
+  },
+};
