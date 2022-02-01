b42e86126d0cc169a835e4e89119f841aade6cb1
Jeongs <kimm.jeongs@gmail.com>
2022-02-02 03:00:34 +0900

    :fire:  Prune: 주석 제거 in Tag

--- src/components/base/Tag/index.tsx
+++ src/components/base/Tag/index.tsx
@@ -5,21 +5,16 @@ import * as S from './Style';
 export interface TagProps {
   activedId: number;
   productId: number;
-  // pointX: number;
-  // pointY: number;
-  // onClick?: (event: React.MouseEvent<HTMLButtonElement>) => void;
 }
 
 const Tag = ({ activedId, productId }: TagProps) => {
   const [isActive] = useCheckSameId(activedId, productId);
 
   return (
-    // <S.TagWrapper pointX={pointX} pointY={pointY}>
     <S.TagImage
       src={isActive ? cancelTag : searchTag}
       alt={isActive ? 'cancelTag' : 'searchTag'}
     />
-    // </S.TagWrapper>
   );
 };
 
