432ca7276557e24a7a8bc8b2b1865179fe174d2e
Jeongs <kimm.jeongs@gmail.com>
2022-02-01 11:13:08 +0900

    :sparkles:  Feat: theme 업데이트 and redux, theme Provider 적용 in index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import { createStore } from 'redux';
 import { Provider } from 'react-redux';
+import store from '@redux/stores';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import { ThemeProvider } from 'styled-components';
 import { GlobalStyle, theme } from '@styles';
 
 ReactDOM.render(
   <React.StrictMode>
-    <GlobalStyle />
-    <ThemeProvider theme={theme}>
-      <App />
-    </ThemeProvider>
+    <Provider store={store}>
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <App />
+      </ThemeProvider>
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,9 +1,20 @@
 const colors = {
-  primary: '#000',
+  primary: '#333',
+  secondary: '#333c45',
+  bold: '#181d1f',
+  border: '#aaafb9',
+  gray: '#898f94',
+  pink: '#ff659e',
+  orange: '#f56b30',
+};
+
+const fonts = {
+  base: '14px',
 };
 
 const theme = {
   colors,
+  fonts,
 };
 
 export type Theme = typeof theme;
