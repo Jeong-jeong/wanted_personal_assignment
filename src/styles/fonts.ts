85cc0c7b40712999dc6d16e60fc600703f532c37
Jeongs <kimm.jeongs@gmail.com>
2022-02-02 15:02:23 +0900

    :lipstick:  UI: fontTooltip 추가 in fonts 폴더

--- src/styles/fonts.ts
+++ src/styles/fonts.ts
@@ -1,13 +1,18 @@
 import { css } from 'styled-components';
 
+export const base = css`
+  font-size: ${({ theme }) => theme.fonts.base};
+  line-height: 1.42857143;
+  color: ${({ theme }) => theme.colors.primary};
+`;
+
 export const fontBadge = css`
   font-size: 11px;
   font-weight: bold;
   line-height: 25px;
 `;
 
-export const base = css`
-  font-size: ${({ theme }) => theme.fonts.base};
-  line-height: 1.42857143;
-  color: ${({ theme }) => theme.colors.primary};
+export const fontTooltip = css`
+  line-height: 1.3em;
+  color: ${({ theme }) => theme.colors.secondary};
 `;
